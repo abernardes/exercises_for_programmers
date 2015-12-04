5b8b1b5fbff247a98af90e361ecfec8eb305d307
André Bernardes <abernardes@gmail.com>
2015-12-03 21:09:28 -0500

    Improve ruby after elixir

--- ruby/01_saying_hello/saying_hello_multiple_greetings.rb
+++ ruby/01_saying_hello/saying_hello_multiple_greetings.rb
@@ -1,12 +1,11 @@
-greetings = [
-  "Nice to meet you, {name}!",
-  "Howdy, {name}!",
-  "Hey, {name}, how's it going?",
-  "Gidday, {name}, pleasure to meet you."
-]
-
 print "Hi, what's your name? "
 name = gets.chomp
 
-greeting = greetings[rand(0..3)].gsub("{name}", name)
-puts greeting
+greetings = [
+  "Nice to meet you, #{name}!",
+  "Howdy, #{name}!",
+  "Hey, #{name}, how's it going?",
+  "Gidday, #{name}, pleasure to meet you."
+]
+
+puts greetings.sample
