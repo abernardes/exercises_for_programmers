81d3cb1e46377ffc6828f997f3c65146634d3792
André Bernardes <abernardes@gmail.com>
2015-12-13 16:50:19 -0500

    Move computations to functions

--- ruby/05_simple_math/simple_math.rb
+++ ruby/05_simple_math/simple_math.rb
@@ -2,20 +2,31 @@ class NegativeNumberError < StandardError; end
 
 begin
   print "What is the first number? "
-  first_number = Integer(gets.chomp)
+  a = Integer(gets.chomp)
   print "What is the second number? "
-  second_number = Integer(gets.chomp)
+  b = Integer(gets.chomp)
 
-  raise NegativeNumberError if first_number < 0 || second_number < 0
+  raise NegativeNumberError if a < 0 || b < 0
 rescue ArgumentError
   puts "ERROR: Please enter a valid number."
 rescue NegativeNumberError
   puts "ERROR: Please enter a positive number."
-end while first_number.nil? || second_number.nil? || first_number < 0 || second_number < 0
+end while a.nil? || b.nil? || a < 0 || b < 0
 
-sum = first_number + second_number
-difference = first_number - second_number
-product = first_number * second_number
-quotient = first_number / second_number
+def sum(a, b)
+  a + b
+end
 
-puts "#{first_number} + #{second_number} = #{sum}\n#{first_number} - #{second_number} = #{difference}\n#{first_number} * #{second_number} = #{product}\n#{first_number} / #{second_number} = #{quotient}"
+def difference(a, b)
+  a - b
+end
+
+def product(a, b)
+  a * b
+end
+
+def quotient(a, b)
+  a / b
+end
+
+puts "#{a} + #{b} = #{sum(a, b)}\n#{a} - #{b} = #{difference(a, b)}\n#{a} * #{b} = #{product(a, b)}\n#{a} / #{b} = #{quotient(a, b)}"
