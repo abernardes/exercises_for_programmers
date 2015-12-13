f8552f7907f5ab77a3b967b8c5e7245dba62766d
André Bernardes <abernardes@gmail.com>
2015-12-13 16:45:47 -0500

    Validate for negatives

--- ruby/05_simple_math/simple_math.rb
+++ ruby/05_simple_math/simple_math.rb
@@ -1,11 +1,17 @@
+class NegativeNumberError < StandardError; end
+
 begin
   print "What is the first number? "
   first_number = Integer(gets.chomp)
   print "What is the second number? "
   second_number = Integer(gets.chomp)
+
+  raise NegativeNumberError if first_number < 0 || second_number < 0
 rescue ArgumentError
   puts "ERROR: Please enter a valid number."
-end while first_number.nil? || second_number.nil?
+rescue NegativeNumberError
+  puts "ERROR: Please enter a positive number."
+end while first_number.nil? || second_number.nil? || first_number < 0 || second_number < 0
 
 sum = first_number + second_number
 difference = first_number - second_number
